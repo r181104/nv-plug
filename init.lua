7a1e0593d1ac139257f8847472263d5b7feef9a9
r181104 <sten181104@gmail.com>
2025-10-23 18:04:20 +0530

    s

--- init.lua
+++ init.lua
@@ -1,18 +1,19 @@
--- Core settings first (safe without plugins)
+-- Core settings first
 require("core.auto")
 require("core.opts")
 require("core.maps")
 
--- Ensure vim-plug location
-local data = vim.fn.stdpath("data")
-local autoload = data .. "/site/autoload/plug.vim"
-if vim.fn.empty(vim.fn.glob(autoload)) > 0 then
-	vim.api.nvim_echo({ { "vim-plug missing: place plug.vim at " .. autoload, "WarningMsg" } }, false, {})
+-- Prefer config/autoload, fall back to data/site/autoload
+local cfg = vim.fn.stdpath("config") .. "/autoload/plug.vim"
+local data = vim.fn.stdpath("data") .. "/site/autoload/plug.vim"
+if vim.fn.empty(vim.fn.glob(cfg)) > 0 and vim.fn.empty(vim.fn.glob(data)) > 0 then
+	vim.api.nvim_echo({ { "vim-plug missing: place plug.vim at " .. cfg .. " or " .. data, "WarningMsg" } }, false, {})
 end
+vim.opt.rtp:prepend(vim.fn.stdpath("config"))
 
--- vim-plug (Lua style)
+-- vim-plug (Lua)
 local Plug = vim.fn["plug#"]
-vim.call("plug#begin", data .. "/plugged")
+vim.call("plug#begin", vim.fn.stdpath("data") .. "/plugged")
 
 Plug("folke/tokyonight.nvim")
 Plug("stevearc/oil.nvim")
@@ -28,7 +29,7 @@ Plug("windwp/nvim-autopairs")
 
 vim.call("plug#end")
 
--- Plugin configs after Plug ends
+-- Plugin configs AFTER plug#end so modules can be required
 require("plug.git")
 require("plug.colors")
 require("plug.notify")
