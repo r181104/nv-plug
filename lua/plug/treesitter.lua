4bf73a701bc7288e050f2d8371bc56c6ad8e473f
r181104 <sten181104@gmail.com>
2025-10-25 03:26:40 +0530

    s

--- lua/plug/treesitter.lua
+++ lua/plug/treesitter.lua
@@ -1,4 +1,8 @@
 require("nvim-treesitter.configs").setup({
+	install = {
+		compilers = { "clang", "gcc" },
+		prefer_git = true,
+	},
 	auto_install = true,
 	highlight = { enable = true, additional_vim_regex_highlighting = true },
 })
